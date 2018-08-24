0f8fd1ed127b4bcb4c3e2288fbe786b1963930de
rend1507 <rendyyofanakusuma@gmail.com>
2018-08-24 10:50:23 +0700

    Revert fixing kode jika tidak ada

--- application/controllers/Materi.php
+++ application/controllers/Materi.php
@@ -43,10 +43,6 @@ class Materi extends CI_Controller
         //      ->set_output(json_encode($data));
 
         
-        //Jika kode pretest tidak ada
-        if($data->kelas == NULL){
-            redirect(base_url());
-        }
 
         $this->load->view('pg_user/materi_pokok', $data);
     }
