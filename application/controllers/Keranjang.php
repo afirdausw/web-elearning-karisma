ac8eb67c2ff37444414c3f0d99f67ed0b73fe5de
Rendy Yofana <rendyyofanakusuma@gmail.com>
2018-12-15 09:31:36 +0700

    Add conditional block on accessing page KERANJANG

--- application/controllers/Keranjang.php
+++ application/controllers/Keranjang.php
@@ -10,37 +10,44 @@ class Keranjang extends CI_Controller
 {
 
     public function index()
-    {
-        $cart = $this->Model_Cart->getCartByIdSiswa($_SESSION['id_siswa']);
-        $data = array(
-            'navbar_links' => $this->Model_pg->get_navbar_links(),
-            'video_demo'   => $this->Model_pg->get_video_demo(null),
-            'judul_tab'    => "Keranjang",
-            'data'         => $cart,
-        );
+    {        
+        if ($this->session->userdata('id_siswa')) {            
+            $cart = $this->Model_Cart->getCartByIdSiswa($_SESSION['id_siswa']);
+            $data = array(
+                'navbar_links' => $this->Model_pg->get_navbar_links(),
+                'video_demo'   => $this->Model_pg->get_video_demo(null),
+                'judul_tab'    => "Keranjang",
+                'data'         => $cart,
+            );
 
 
-        $this->load->view('pg_user/keranjang', $data);
+            $this->load->view('pg_user/keranjang', $data);
+        } else {
+            redirect(base_url("login"));
+        }
     }
 
     public function add($id_mapel)
     {
+        if ($this->session->userdata('id_siswa')) {            
+            $mapel = $this->Model_Cart->getCartByIdSiswaIdMapel($_SESSION['id_siswa'], $id_mapel);
 
-        $mapel = $this->Model_Cart->getCartByIdSiswaIdMapel($_SESSION['id_siswa'], $id_mapel);
-
-        if (count($mapel) <= 0) {
-            $create = $this->Model_Cart->addCartSiswa($_SESSION['id_siswa'], $id_mapel);
-            if ($create) {
-
-                $cart = $this->Model_Cart->getCartByIdSiswa($_SESSION['id_siswa']);
-                $cart = obj_to_arr($cart);
-                $jumlah_cart = count($cart);
-                $this->session->set_userdata('cart', $cart);
-                $this->session->set_userdata('jumlah_cart', $jumlah_cart);
+            if (count($mapel) <= 0) {
+                $create = $this->Model_Cart->addCartSiswa($_SESSION['id_siswa'], $id_mapel);
+                if ($create) {
+    
+                    $cart = $this->Model_Cart->getCartByIdSiswa($_SESSION['id_siswa']);
+                    $cart = obj_to_arr($cart);
+                    $jumlah_cart = count($cart);
+                    $this->session->set_userdata('cart', $cart);
+                    $this->session->set_userdata('jumlah_cart', $jumlah_cart);
+                }
             }
+    
+            redirect($_SESSION['RedirectKe']);
+        } else {
+            redirect(base_url("login"));
         }
-
-        redirect($_SESSION['RedirectKe']);
 
     }
 
