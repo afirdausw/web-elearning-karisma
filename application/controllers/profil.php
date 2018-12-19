8d585362a719cff6ec1ddc7158320fae5664e2d0
Dony Rinaldo Radjah <donyradjah@gmail.com>
2018-12-19 16:00:03 +0700

    Revert "forget what i did"
    This reverts commit a4da5a26982aafe817c30d0e6840934f4c950bb5.

--- application/controllers/profil.php
+++ application/controllers/profil.php
@@ -18,52 +18,52 @@ class profil extends CI_Controller
     public function index()
     {
         $kelas_navbar = $this->Model_pg->fetch_all_kelas();
-        if ($this->session->userdata('id_siswa') != NULL) {
-            $siswa = $this->Model_pg->get_data_user($this->session->userdata('id_siswa'));
+    	if($this->session->userdata('id_siswa') != NULL){
+	    	$siswa = $this->Model_pg->get_data_user($this->session->userdata('id_siswa'));
             $log_baca = $this->Model_pg->get_log_baca_detail($this->session->userdata('id_siswa'));
-            $data = array(
-                'siswa'        => $siswa,
-                'log_baca'     => $log_baca,
-                "kelas_navbar" => $kelas_navbar,
-            );
+	    	$data = array(
+	            'siswa' => $siswa,
+                'log_baca' => $log_baca,
+                "kelas_navbar" => $kelas_navbar, 
+	        );
 
-            $this->load->view('pg_user/profil-siswa', $data);
+	        $this->load->view('pg_user/profil-siswa', $data);
+            
+    	}else if($this->session->userdata('pretest_id') != NULL){
+	    	$pretest = $this->Model_profil->get_data_user_pretest($this->session->userdata('pretest_id'));
+	    	$data = array(
+	            'pretest' => $pretest,
+                "kelas_navbar" => $kelas_navbar, 
+	        );
 
-        } else if ($this->session->userdata('pretest_id') != NULL) {
-            $pretest = $this->Model_profil->get_data_user_pretest($this->session->userdata('pretest_id'));
-            $data = array(
-                'pretest'      => $pretest,
-                "kelas_navbar" => $kelas_navbar,
-            );
-
-            $this->load->view('pg_user/profil-pretest', $data);
-
-        } else {
+	        $this->load->view('pg_user/profil-pretest', $data);
+            
+    	}else{
             redirect(base_url(), 'refresh');
-        }
+    	}
     }
 
 
     public function premium()
     {
-        if ($this->session->userdata('id_siswa') != NULL) {
+        if($this->session->userdata('id_siswa')!=NULL){
             $siswa = $this->Model_pg->get_data_user($this->session->userdata('id_siswa'));
 
             $data = [
-                "id_siswa" => $this->session->userdata('id_siswa'),
+                "id_siswa"     => $this->session->userdata('id_siswa'),
             ];
-            if ($siswa->id_premium == 0) {
+            if ($siswa->id_premium == 0){
                 $data["id_premium"] = 1;
-            } else if ($siswa->id_premium >= 0) {
+            }else if ($siswa->id_premium >= 0){
                 $data["id_premium"] = 0;
             }
             $update = $this->Model_profil->toogle_premium($data);
-            if ($update) {
-                redirect(base_url() . 'profil');
-            } else {
+            if($update){
+                redirect(base_url().'profil');
+            }else{
                 var_dump($update);
             }
-        } else {
+        }else{
             redirect(base_url(), 'refresh');
         }
 
