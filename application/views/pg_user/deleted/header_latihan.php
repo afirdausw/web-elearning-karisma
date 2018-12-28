e89fdd5618a030066ed1bdf54f15cb9f5046ccf1
Rendy Yofana <rendyyofanakusuma@gmail.com>
2018-12-28 13:26:55 +0700

    Deleting php Files
    - Dengan pemanggilan CSS yang ada di head setiap PHP, karena template head sudah ada

--- application/views/pg_user/deleted/header_latihan.php
+++
@@ -1,6 +0,0 @@
-<?php $kondisi_flag=0; ?>
-<nav class="navbar navbar-default navbar-page" style="padding-left:0; padding-right:0;" role="navigation">
-  <div class="container">
-    <a class="navbar-brand center-block" href="<?php echo base_url(); ?>"></a>
-  </div>
-</nav>
