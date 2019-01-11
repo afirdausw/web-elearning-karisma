402920561164f6358549b6d2f54a7149da504855
Rendy Yofana <rendyyofanakusuma@gmail.com>
2019-01-11 16:57:33 +0700

    Lokasi jquery di footer + deleting CHART.js

--- application/views/pg_admin/materi/materi_pokok_form.php
+++ application/views/pg_admin/materi/materi_pokok_form.php
@@ -116,8 +116,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  <script type="text/javascript">
    $(document).ready(function(){
 
-     demo.initChartist();
-
      $.notify({
        icon: 'pe-7s-gift',
        message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
