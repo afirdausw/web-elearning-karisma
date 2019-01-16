0c6db4937ef692e125f4b3edf73634eea9a3b2dc
Rendy Yofana <rendyyofanakusuma@gmail.com>
2019-01-16 11:18:55 +0700

    Merge branch master 1/8/2019
    fix empty cart header

--- application/views/pg_user/inc/header.php
+++ application/views/pg_user/inc/header.php
@@ -37,8 +37,7 @@
 		}
 		#wrap {
 			min-height: 100%;
-		}		
-		/* Footer optimization */
+		}
 	</style>
 </head>
 
@@ -115,22 +114,12 @@ if ($this->uri->segment(1) != '') { ?>
 
                 <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">Beranda <span class="arrow-down ti-angle-down"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Link</a></li>
-                                <li><a href="#">Link</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">Tentang Kami <span class="arrow-down ti-angle-down"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Link</a></li>
-                                <li><a href="#">Link</a></li>
-                            </ul>
-                        </li>
+                        <li>
+							<a href="<?php echo base_url(); ?>" role="button"aria-expanded="false">Beranda</span></a>
+						</li>
+						<li>
+							<a href="#" role="button" aria-expanded="false">Tentang Kami</a>
+						</li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                                aria-expanded="false">Kursus <span class="arrow-down ti-angle-down"></span></a>
@@ -182,27 +171,38 @@ if ($this->uri->segment(1) != '') { ?>
                                             class="badge"><?= count($cart) ?></span>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-right min-width-320"
-                                    aria-labelledby="dropdownMenuCart">
+                                    aria-labelledby="dropdownMenuCart" style="padding: 0px;">
                                     <?php
+                                    if ($numCart == 0) {
+                                        ?>
+                                        <li class="cart-dropdown" style="display: block;">
+                                            <div style="white-space: normal;" class="pull-right w-100 pl-3 mt-2 text-center">
+                                                <h4 class="w-100 mt-0 cart-judul">Keranjang anda Kosong</h4> 
+                                                <br>
+                                                <span><small>Silahkan anda pilih produk yang kita miliki.</small></span>
+                                            </div>
+                                        </li>	
+                                        <?php
+                                    } else {
                                     foreach ($cart as $key => $value) {
                                         ?>
                                         <li class="cart-dropdown" style="display: block;">
                                             <a href="#">
-                                                <div class="pull-left w-25  mt-2">
-                                                    <img class="w-100 my-auto"
+                                                <div class="pull-left w-25 mt-2">
+													<img class="w-100 thumb-cart"
                                                          src="<?= base_url() ?>image/mapel/<?= $value['gambar_mapel'] ?>">
                                                 </div>
                                                 <div style="white-space: normal;" class="pull-right w-75 pl-3  mt-2">
-                                                    <h4 class="w-100 mt-0"><?= $value['nama_mapel'] ?></h4>
+                                                    <h4 class="w-100 mt-0 font-weight-bold cart-judul"><?= $value['nama_mapel'] ?></h4>
                                                     <h5 class="w-100">
-                                                        <!--                                                        <span class="mr-3 text-gray text-line-through font-w700">Rp. 1.200.000</span>-->
-                                                        <span class="font-weight-bold">Rp. <?= money($value['harga']) ?></span>
+                                                        <small><span class="font-italic">Rp. <?= money($value['harga']) ?></span></small>
                                                     </h5>
                                                 </div>
                                             </a>
                                         </li>
-                                    <?php } ?>
-                                    <li class="text-center">
+                                    <?php }
+                                    }?>
+                                    <li class="text-center" style="margin-top: 80px;">
                                         <a class="mt-2" style="display: block;" href="<?= base_url('keranjang') ?>">Checkout Cart</a>
                                     </li>
                                 </ul>
