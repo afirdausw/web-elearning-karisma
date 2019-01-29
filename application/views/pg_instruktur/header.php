eb71eb158fc3d31c12c7b52a31c9c925c5ebecd2
Aris <arisabdul04@gmail.com>
2019-01-29 13:20:42 +0700

    View Login Instruktur

---
+++ application/views/pg_instruktur/header.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?= ((isset($judul_tab) AND $judul_tab != '') ? $judul_tab . " - " : "") ?>Karisma Academy - Sertifikasi
+        Online</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="cache-control" content="max-age=0"/>
+    <meta http-equiv="cache-control" content="no-cache"/>
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT"/>
+    <meta http-equiv="pragma" content="no-cache"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="<?php echo base_url('assets/dashboard/images/favicon.ico'); ?>">
+    <link rel="icon" sizes="130x128" href="<?php echo base_url('assets/dashboard/images/favicon.ico'); ?>">
+    <link rel="apple-touch-icon" sizes="130x128" href="<?php echo base_url('assets/dashboard/images/favicon.ico'); ?>">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pg_user/css/themify-icons.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pg_user/css/font-awesome.min.css'); ?>">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pg_user/css/bootstrap.css'); ?>">
+    <link rel="stylesheet" type="text/css"
+          href="<?php echo base_url('assets/pg_user/css/jquery.mCustomScrollbar.min.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pg_user/css/style2.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pg_user/css/custom.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pg_user/css/responsive.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/pg_user/css/bs-4-margin.css'); ?>">
+    <style>
+        /* Footer optimization */
+        html, body {
+            height: 77.3%;
+        }
+
+        #wrap {
+            min-height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+<div id="wrap">
+    <div id="main" class="clear-top">
+        <div id="navtop" style="opacity:0;width:0;height:0"></div>
+        <?php
+        if ($this->uri->segment(1) != '') { ?>
+            <div class="overlay"></div>
+            <nav id="sidebar">
+                <div id="wrap-sidebar">
+                    <div id="dismiss">
+                        <span class="ti-close"></span>
+                    </div>
+
+                    <div class="sidebar-header">
+                        <img src="<?php echo base_url('assets/pg_user/images/header-logo.png') ?>" width="250px"
+                             height="38px"
+                             alt="Karisma Academy">
+                    </div>
+
+                    <ul class="list-unstyled components">
+                        <li><a href="#">Beranda</a></li>
+                        <li><a href="#">Kelas</a></li>
+                        <li><a href="#">Quiz</a></li>
+                        <li><a href="#">Tanya Jawab</a></li>
+                    </ul>
+                </div>
+            </nav> <!-- Sidebar Menu -->
+
+            <?php
+        }
+        ?>
+
+        <nav id="sidebar">
+            <div id="wrap-sidebar">
+                <div id="dismiss">
+                    <span class="ti-close"></span>
+                </div>
+
+                <div class="sidebar-header">
+                    <img src="<?php echo base_url('assets/pg_user/images/header-logo.png') ?>" width="250px"
+                         height="38px"
+                         alt="Karisma Academy">
+                </div>
+
+                <ul class="list-unstyled components">
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Kelas</a></li>
+                    <li><a href="#">Quiz</a></li>
+                    <li><a href="#">Tanya Jawab</a></li>
+                </ul>
+            </div>
+        </nav> <!-- Sidebar Menu -->
+        <!-- HEADER -->
+        <header class="top-header" <?php if ($this->uri->segment(1) == '') { ?> style="position:sticky;top:5;" <?php } ?>>
+            <div class="container">
+                <nav class="navbar navbar-default navbar-custom" role="navigation">
+                    <div class="">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                    data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <?php
+                            if ($this->uri->segment(1) != '') { ?>
+                                <button type="button" id="sidebarCollapse" class="open-sidebar">
+                                    <span class="ti-menu"></span>
+                                </button>
+                            <?php } ?>
+                            <a class="navbar-brand logo" href="<?php echo base_url(); ?>"></a>
+                        </div>
+
+                        <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li>
+                                    <a href="<?php echo base_url(); ?>" role="button"
+                                       aria-expanded="false">Beranda</a>
+                                </li>
+                                <li>
+                                    <a href="#" role="button" aria-expanded="false">Tentang Kami</a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                       aria-expanded="false">Kursus <span class="arrow-down ti-angle-down"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <?php
+                                        $kelas_navbar = $this->Model_pg->fetch_all_kelas();
+                                        foreach ($kelas_navbar as $key) :
+                                            echo "<li><a href='" . base_url("kelas/$key->id_kelas") . "'>$key->alias_kelas</a> </li>";
+                                        endforeach;
+                                        ?>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                       aria-expanded="false">Sertifikat <span
+                                                class="arrow-down ti-angle-down"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Daftar</a></li>
+                                        <li><a href="#">Login</a></li>
+                                    </ul>
+                                </li>
+                                <?php
+                                $idsiswa = $this->session->userdata('id_siswa');
+                                if ($idsiswa != NULL) {
+                                    $transaksi_belum_di_bayar = $this->Model_Transaksi->getTransaksiByIdSiswaAndStatus($idsiswa, 0);
+
+                                    $jml_belum_di_bayar = count($transaksi_belum_di_bayar);
+                                    $siswa = $this->Model_pg->get_data_user($idsiswa);
+                                    ?>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                           aria-expanded="false"><?= $siswa->nama_siswa ?> <span data-toggle="tooltip"
+                                                                                                 data-placement="bottom"
+                                                                                                 title=" <?= $jml_belum_di_bayar ?> Transaksi Belum Di bayar"
+                                                                                                 class="badge"><?= $jml_belum_di_bayar ?></span>
+
+
+                                            <span class="arrow-down ti-angle-down"></span></a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a href="<?= base_url('profil') ?>">Profil</a></li>
+                                            <li><a href="<?= base_url('history-transaksi') ?>">History Transaksi <span
+                                                            data-toggle="tooltip"
+                                                            data-placement="bottom"
+                                                            title=" <?= $jml_belum_di_bayar ?> Transaksi Belum Di bayar"
+                                                            class="badge"><?= $jml_belum_di_bayar ?></span></a></li>
+                                            <li role="separator" class="divider"></li>
+                                            <li><a href="<?php echo base_url('login/logout') ?>">Logout</a></li>
+                                        </ul>
+                                    </li>
+                                    <?php
+                                    $cart = $this->Model_Cart->getCartByIdSiswa($_SESSION['id_siswa']);
+                                    $cart = obj_to_arr($cart);
+                                    $numCart = count($cart);
+                                    ?>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                           aria-expanded="false">
+                                            <i class="ti-shopping-cart"></i><span
+                                                    class="badge"><?= $numCart ?></span>
+                                        </a>
+                                        <ul class="dropdown-menu dropdown-menu-right min-width-320"
+                                            aria-labelledby="dropdownMenuCart" style="padding: 0px;">
+                                            <?php
+                                            if ($numCart == 0) {
+                                                ?>
+                                                <li class="cart-dropdown" style="display: block;">
+                                                    <div style="white-space: normal;"
+                                                         class="pull-right w-100 pl-3 mt-2 text-center">
+                                                        <h4 class="w-100 mt-0 cart-judul">Keranjang anda Kosong</h4>
+                                                        <br>
+                                                        <span><small>Silahkan anda pilih produk yang kita miliki.</small></span>
+                                                    </div>
+                                                </li>
+                                                <?php
+                                            } else {
+                                                foreach ($cart as $key => $value) {
+                                                    ?>
+                                                    <li class="cart-dropdown" style="display: block;">
+                                                        <a href="#">
+                                                            <div class="pull-left w-25 mt-2">
+                                                                <img class="w-100 thumb-cart"
+                                                                     src="<?= base_url() ?>image/mapel/<?= $value['gambar_mapel'] ?>">
+                                                            </div>
+                                                            <div style="white-space: normal;"
+                                                                 class="pull-right w-75 pl-3  mt-2">
+                                                                <h4 class="w-100 mt-0 font-weight-bold cart-judul"><?= $value['nama_mapel'] ?></h4>
+                                                                <h5 class="w-100">
+                                                                    <!--                                                        <span class="mr-3 text-gray text-line-through font-w700">Rp. 1.200.000</span>-->
+                                                                    <small><span
+                                                                                class="font-italic">Rp. <?= money($value['harga']) ?></span>
+                                                                    </small>
+                                                                </h5>
+                                                                <!-- <hr> -->
+                                                            </div>
+                                                        </a>
+                                                    </li>
+                                                    <?php
+                                                }
+                                            }
+                                            ?>
+                                            <li class="text-center" style="margin-top: 85px;">
+                                                <a class="mt-2 border-top" style="display: block;"
+                                                   href="<?= base_url('keranjang') ?>">Checkout Cart</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                <?php } else if ($this->session->userdata('pretest_logged_in')) { ?>
+
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                           aria-expanded="false"><?= ($this->session->userdata('pretest_nama') != NULL ? $this->session->userdata('pretest_nama') : "Anonim"); ?>
+                                            <span class="ti-face-sad" style="color:red;" data-toggle="tooltip"
+                                                  data-placement="bottom"
+                                                  title="Anda hanya terdaftar sebagai siswa pretest"></span> <span
+                                                    class="arrow-down ti-angle-down"></span></a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a href="<?= base_url('profil') ?>">Profil</a></li>
+                                            <li role="separator" class="divider"></li>
+                                            <li><a href="<?= base_url("pretest/logout") ?>">Logout</a></li>
+                                        </ul>
+                                    </li>
+
+                                <?php } else { ?>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                           aria-expanded="false">Login <span
+                                                    class="arrow-down ti-angle-down"></span></a>
+                                        <ul class="dropdown-menu" role="menu">
+                                            <li><a href="<?php echo base_url() . 'signup' ?>">Daftar</a></li>
+                                            <li><a href="<?php echo base_url() . 'login' ?>">Login</a></li>
+                                        </ul>
+                                    </li>
+                                    <?php
+                                } ?>
+                            </ul>
+                        </div>
+                        <!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.container-fluid -->
+                </nav>
+            </div>
+        </header>
--- application/views/pg_instruktur/login_page.php
+++ application/views/pg_instruktur/login_page.php
@@ -1,100 +1,42 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
+<?php include("header.php"); ?>
 
-<?php include "html_header.php"; ?>
 
-<style type="text/css">
-	@media(max-width:767px;){
-		.login-page{
-			margin-top:40px;
-		}
-	}
-	.login-page{
-		margin-top:100px;
-	}
-</style>
-
-<div class="wrapper">
-		<div class="content">
-			<div class="container-fluid">
-				<div class="row-fluid">
-					<div class="col-xs-12 col-sm-4 col-sm-offset-4 login-page">
-					
-					 <?php echo $this->session->flashdata('alert'); ?>
-					
-						<div class="card">
-							<div class="header">
-								<h4 class="title"><?php echo isset($page_title) ? $page_title : "Login"?>
-									<?php if($this->uri->segment(1)== "psep_sekolah"){
-										echo "Sekolah / Guru";
-									}else{
-										echo "Admin";
-									}
-
-									?>
-									
-								</h4>
-							</div>
-							<div class="content">
-								<form method="post" action="<?php echo $form_action?>">
-				
-									<div class="row">
-										<div class="col-md-12">
-											<div class="form-group">
-												<div class="input-group">
-													<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-													<input type="text" name="username" id="username" class="form-control" placeholder="Username" required="required" autofocus="true">
-												</div>
-												<?php echo form_error('username', '<div class="text-danger">', '</div>'); ?>
-											</div>
-										</div>
-									</div>
-				
-									<div class="row">
-										<div class="col-md-12">
-											<div class="form-group">
-												<div class="input-group">
-													<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-													<input type="password" name="userpass" id="userpass" class="form-control" placeholder="Password" required="required">
-												</div>
-												<?php echo form_error('userpass', '<div class="text-danger">', '</div>'); ?>
-											</div>
-										</div>
-									</div>
-								
-									<div class="row">
-										<div class="col-sm-12">
-											<button type="submit" name="form_submit" value="submit" class="btn btn-fill btn-turquoise form-control">
-												<i class="glyphicon glyphicon-log-in"></i> Login
-											</button>
-										</div>
-									</div>
-								</form>
-
-								<div class="footer text-right">
-									<hr>
-									<div class="stats">
-										<i class="fa fa-copyright"></i> <?php echo date('Y');?> <span class="text-turquoise">Lembaga Pendidikan Islam Hidayatullah</span>
-									</div>
-								</div>
-							</div>
-						</div>
+<section class="mt-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-5 col-xs-12 col-center login p-5">
+				<?php echo $this->session->flashdata('alert'); ?>
+				<h2>Login Instruktur</h2>
+				<form method="post" action="<?php echo $form_action ?>" class="mt-5">
+					<div class="form-group has-feedback">
+						<span class="ti-user form-control-feedback"></span>
+						<input type="text" name="username" id="username" class="form-control" placeholder="Username" required="required" autofocus="true">
 					</div>
-				</div>
+					<div class="form-group has-feedback">
+						<span class="ti-lock form-control-feedback"></span>
+						<input type="password" name="userpass" id="userpass" class="form-control" placeholder="Password" required="required">
+					</div>
+					<button type="submit" name="form_submit" value="submit" class="btn btn-primary btn-block btn-login">Login</button>
+					<div class="mt-2">
+                    	<a href='#'>Lupa Password?</a>
+                    </div>
+				</form>
 			</div>
-		</div> <!-- end .content -->
-
+		</div>
 	</div>
-</div> <!-- end .wrapper -->
+</section>
+
+<!-- Footer -->
+<?php // include('modal_lupa_password.php'); ?>
+<?php include('footer.php'); ?>
+<!-- /Footer -->
+
+<!-- Javascript -->
+<script type="text/javascript" src="<?php echo base_url('assets/pg_user/js/jquery-1.11.3.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/pg_user/js/bootstrap.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/pg_user/js/megamenu.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/pg_user/js/form-validator/formValidation.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/pg_user/js/form-validator/bootstrap.js'); ?>"></script>
 
 </body>
-
-	<!--   Core JS Files   -->
-	<script src="<?php echo base_url('assets/js/jquery-3.2.1.js');?>" type="text/javascript"></script>
-	<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>" type="text/javascript"></script>
-
-	<!--  Notifications Plugin    -->
-	<script src="<?php echo base_url('assets/js/bootstrap-notify.js');?>"></script>
-
 </html>
